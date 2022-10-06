f41d127ccde318d5a2af693ebd1cd47c58c803dc
Aleksandar Maksimovic <aleksandar.maksimovic@p3-group.com>
2022-10-06 17:20:37 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  CssBaseline,
+  GlobalStyles,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { theme } from './theme';
+
+const GLOBAL_STYLES = {
+  body: {
+    margin: 0,
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={GLOBAL_STYLES} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Typography variant="h1">Starwars</Typography>}
+        />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+import '@fontsource/space-grotesk/300.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/500.css';
+import '@fontsource/space-grotesk/700.css';
+
+export const theme = createTheme({
+  typography: { fontFamily: 'Space Grotesk' },
+});
